acfb405e12a6f7329a1e88f85e7db6d4fba33aba
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-06-14 17:29:23 -0400

    Add morse code

---
+++ morse/index.js
@@ -0,0 +1,22 @@
+const morse = require("morse-code-converter");
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const plaintext = req.query.plaintext;
+
+    let code = undefined
+
+    if (typeof plaintext === 'undefined' || plaintext === "") {
+        code = "Please enter some text to convert!"
+    } else {
+        code = morse.textToMorse(plaintext);
+    }
+
+    const responseMessage = code
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
