a5badddf456280adb0e654afc219e27386260359
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-07-07 14:40:29 -0400

    Add support for image download

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -10,12 +10,13 @@ bunnForm.addEventListener('submit', async function (event) {
    }
 
    var payload = new FormData(bunnForm);
-   const endpoint = "https://jonathanrogersbitprojectserverless2022.azurewebsites.net/api/bunnimage-upload?code=yoYzMMIxkrUHq6kcSe5g3ZH5c2ib6rF8PhrQdhD6qQLkAzFuqQtaiQ%3D%3D"
+   const endpoint = "https://jonathanrogersbitprojectserverless2022.azurewebsites.net/api/bunnimage-upload?code=yoYzMMIxkrUHq6kcSe5g3ZH5c2ib6rF8PhrQdhD6qQLkAzFuqQtaiQ=="
    const options ={
         "method": "POST",
         "body": payload,
         headers: {
-            "codename": username
+            'username':username,
+            'Content-Type': 'multipart/form-data'
         }
     }
    const resp = await fetch(endpoint, options)
@@ -23,4 +24,26 @@ bunnForm.addEventListener('submit', async function (event) {
    output.textContent = "Your image has been stored successfully."
 
    output.textContent = username + "❤" 
+});
+
+const downloadButton = document.getElementById('button1');
+
+downloadButton.addEventListener("click", async function (event) {
+   var username = document.getElementById("downloadUsername").value;
+   console.log("attempting to download your image...")
+
+   const url = "https://jonathanrogersbitprojectserverless2022.azurewebsites.net/api/bunnimage-download?code=7UOFJ2QTt_y7GjxbWdKGfO2HphsdvKQeSainV_8zQdnGAzFuqv0B4w==";
+
+   const resp = await fetch(url, {
+      method: "GET",
+      headers: {
+         username:username,
+      },
+   });
+
+   const data = await resp.json();
+   console.log("Image has been received.")
+   console.log(data)
+
+   window.open(date.downloadUri, "_self");
 });
