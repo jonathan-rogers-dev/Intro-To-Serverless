958c618c84df4831d97beacfd2ff5ea76b7d60e8
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2022-07-08 15:25:56 -0400

    Add support for more image

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,18 +3,9 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    function getName() {
-        let names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
-        let random_value = Math.floor(names.length * Math.random())
-        console.log(random_value)
-        let resultname = names[random_value]
-        console.log(resultname)
+    async function getImage(name) {
 
-        return resultname
-    }
-    
-    async function getImage() {
-        let resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
+        let resp = await fetch(`https://bit-cat.azurewebsites.net/cat/says/${name}`, {
             method: 'GET'
         });
 
@@ -26,16 +17,14 @@ module.exports = async function (context, req) {
 
     }
 
-    var firstcat = await getImage();
-    var secondcat = await getImage();
-    var name1 = await getName();
-    var name2 = await getName();
+    let nameParms = req.query.name;
+
+    var cat = await getImage(nameParms);
 
     context.res = {
         body: {
-            cat1: firstcat,
-            cat2: secondcat,
-            names: [name1, name2]
+            cat: cat,
+            // names: [name1, name2]
         }
     }
 }
